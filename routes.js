c926f9b509e9c70f8faa064e312e50f50feb21df
Brian Zelip <bzelip@gmail.com>
2018-10-12 00:21:37 -0400

    use cors in routes.js

--- routes.js
+++ routes.js
@@ -2,9 +2,10 @@ const express = require('express');
 const router = express.Router();
 const controller = require('./controller');
 const { catchErrors } = require('./errorHandlers');
+const cors = require('cors');
 
 router.get('/', controller.home);
 
-router.get('/get', catchErrors(controller.getAllItems));
+router.get('/get', cors(), catchErrors(controller.getAllItems));
 
 module.exports = router;
