a354af0c8178ba0a65b7ad292a32aa7324b8ff3b
Brian Zelip <bzelip@gmail.com>
2019-02-15 15:13:46 -0500

    routes.js: Add /create post route

--- routes.js
+++ routes.js
@@ -9,6 +9,8 @@ router.get('/get', catchErrors(controller.getAllItems));
 
 router.post('/submit', controller.submit);
 
+router.post('/create', catchErrors(controller.createItem));
+
 router.get('/test', controller.test);
 
 module.exports = router;
