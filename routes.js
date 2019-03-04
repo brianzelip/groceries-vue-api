4f10986a4d774529476fed9eea8c15696a399a06
Brian Zelip <bzelip@gmail.com>
2019-03-04 13:47:30 -0500

    routes: Create new route

--- routes.js
+++ routes.js
@@ -11,4 +11,6 @@ router.post('/submit', controller.submit);
 
 router.post('/create', catchErrors(controller.createItem));
 
+router.post('/edit/:_id', catchErrors(controller.updateItem));
+
 module.exports = router;
