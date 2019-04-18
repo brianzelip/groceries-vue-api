1de914ab9b3f36ed32c2cd4db515e6c5892dac8e
Brian Zelip <bzelip@gmail.com>
2019-04-18 11:12:38 -0400

    Update controller.js

--- controller.js
+++ controller.js
@@ -17,9 +17,10 @@ exports.getAllItems = async (req, res) => {
 exports.submit = (req, res) => {
   const html = req.body.html;
   const recipients = req.body.recipients;
-  const to = recipients.map(
-    r => process.env[`${r.slice(0, 1).toUpperCase()}Z`]
-  );
+//   const to = recipients.map(
+//     r => process.env[`${r.slice(0, 1).toUpperCase()}Z`]
+//   );
+  const to = recipients;//this is a hack for rushing groceries-vue out the shipping door
 
   const transporter = nodemailer.createTransport({
     host: 'smtpout.secureserver.net',
